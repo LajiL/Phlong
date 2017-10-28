3bfb7d25fc3b5829381d9690b5760ae0f1c52e89
Ricky <ricky.xiao331@gmail.com>
2017-10-28 20:53:23 +0800

    添加加载等待画面

--- app/src/main/java/com/lajilao/phlong/UserFragment.java
+++ app/src/main/java/com/lajilao/phlong/UserFragment.java
@@ -1,11 +1,15 @@
 package com.lajilao.phlong;
 
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 /**
@@ -13,17 +17,32 @@ import android.widget.TextView;
  */
 public class UserFragment extends Fragment {
 
-
-    public UserFragment() {
-        // Required empty public constructor
-    }
-
+    private View view;
+    private ProgressBar progressBar;
+    public UserFragment() {}
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.user_fragment,container,false);
+        view = inflater.inflate(R.layout.user_fragment,container,false);
         return view;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+//        progressBar = (ProgressBar) view.findViewById(R.id.user_progress_bar);
+//        progressBar.setVisibility(View.GONE);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
     }
 
 }
